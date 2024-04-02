55f80c9c8513289dab58152bcd59311dff1298ef
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-02 12:59:07 -0600

    more changes

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) =>{
-    res.json('foo')
+    
 })
 router.get('/', (req, res) =>{
 
@@ -20,9 +20,5 @@ router.put('/:id', (req, res) =>{
 router.get('/:id/message', (req, res) =>{
 
 })
-router.verb('/', (req, res) =>{
-
-})
-
 
 module.exports = router
